2bd117bb58c111ac7ec6c3555ab73cb3de699120
daniilvasutin <daniilvasutin@gmail.com>
2013-10-01 20:49:34 +0300

    Add allMonsterPath

--- src/js/tower.js
+++ src/js/tower.js
@@ -1,33 +1,22 @@
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 600;
-canvas.height = 400;
-document.body.appendChild(canvas);
+var canvas, context, toggle;
 
 var images = {};
-var monster = {};
-monster.x = 0;
-monster.y = 200;
+var monster = {
+     x: 0,
+     y: 200
+};
 
-function monsterMove(){
-
-    if(monster.x < 65 && monster.y > 72){
-        monster.x = monster.x + 32;
-    }
-    if(monster.x > 64 && monster.y > 71)
-    {
-        monster.y = monster.y - 32;
-    }
-    if(monster.y < 72 )
-    {
-        monster.x = monster.x + 32;
-    }
-    render();
-}
+var coordinates = [
+    [0, 200], //0
+    [80,75],  //1
+    [205,235],//2
+    [370,165],//3
+    [570,165] //4
+];
 
 function render(){
-    ctx.drawImage(images.bg, 0, 0);
-    ctx.drawImage(images.monster, monster.x, monster.y);
+    context.drawImage(images.bg, 0, 0);
+    context.drawImage(images.monster, monster.x, monster.y);
 }
 
 function loadImages(sources) {
@@ -54,14 +43,81 @@ var sources = {
 };
 
 function showImages(){
-    ctx.drawImage(images.bg, 0, 0);
+    context.drawImage(images.bg, 0, 0);
 }
 
-window.addEventListener("DOMContentLoaded", function()
-{
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        window.oRequestAnimationFrame      ||
+        window.msRequestAnimationFrame     ||
+        function(callback){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+window.cancelRequestAnimFrame = ( function() {
+    return window.cancelAnimationFrame              ||
+        window.webkitCancelRequestAnimationFrame    ||
+        window.mozCancelRequestAnimationFrame       ||
+        window.oCancelRequestAnimationFrame         ||
+        window.msCancelRequestAnimationFrame        ||
+        clearTimeout
+})();
+
+init();
+render();
+setTimeout(animate, 1000);
+
+function animate() {
+
+    monsterMove();
+
+    // request new frame
+    var req = requestAnimFrame(animate);
+
+    if(monster.x+32 >= canvas.width){
+        cancelRequestAnimFrame(req);
+    }
+}
+
+function monsterMove(){
+
+    if(monster.x < coordinates[1][0]+1 && monster.y == coordinates[0][1]){
+        monster.x += 1;
+    }
+    if(monster.x == coordinates[1][0] && monster.y > coordinates[1][1]){
+        monster.y -= 1;
+    }
+    if(monster.x < coordinates[2][0]+1 && monster.y == coordinates[1][1]){
+        monster.x += 1;
+    }
+    if(monster.x == coordinates[2][0] && monster.y < coordinates[2][1]){
+        monster.y += 1;
+    }
+    if(monster.x < coordinates[3][0]+1 && monster.y == coordinates[2][1]){
+        monster.x += 1;
+    }
+    if(monster.x == coordinates[3][0] && monster.y > coordinates[3][1]){
+        monster.y -= 1;
+    }
+    if(monster.x < coordinates[4][0] && monster.y == coordinates[3][1] && monster.x >= coordinates[3][0]){
+        monster.x += 1;
+    }
+
+    console.log(monster.x + " : " + monster.y);
+
+    render();
+}
+
+function init() {
+    canvas = document.createElement("canvas");
+    context = canvas.getContext("2d");
+    canvas.width = 600;
+    canvas.height = 400;
+
+    document.body.appendChild(canvas);
+
     loadImages(sources);
-    setInterval(monsterMove, 300);
-});
-
-
-
+}
