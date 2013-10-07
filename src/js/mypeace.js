5a376ae86da088c2fc844c4568d613424c41283e
alexvaganov <alex@datacorp.ru>
2013-10-07 22:44:47 +0300

    2 types of tower created

--- src/js/mypeace.js
+++ src/js/mypeace.js
@@ -1,14 +1,11 @@
 ﻿//Global variables are defined here
 var canvas, ctx;
 
-var drp=false;          // true - если перетаскиваем
-var pozx=0; var pozy=0; // координаты начала отображения изображения
-var smx=0;   var smy=0;   // координаты мыши - нажали на кнопку
-var emx=0;   var emy=0;   // координаты мыши - опустили на кнопку
-
+var build = false;          //true - if tower is building
 
 var newTower;
-var images = {}; // images object
+var towerType = null;
+var images = {}; //images object
 var towersArray = Array(); // towers array
 
 //All images and path are defined here
@@ -18,14 +15,15 @@ var sources = {
     newMonster: "../images/mo1.png",
 	flameTowerIco: "../images/flame_tower_ico.png",
 	frostTowerIco: "../images/frost_tower_ico.png",
-	flameTower: "../images/flame_tower.png"
+	flameTower: "../images/flame_tower.png",
+	frostTower: "../images/frost_tower.png"
 };
 
 //Load images from sources to canvas
 function loadImages(sources) {
     var loadedImages = 0;
     var numImages = 0;
-    // get num of images in sources
+    //get num of images in sources
     for(var src in sources) {
         numImages++;
     }
@@ -48,17 +46,17 @@ function initialization() {
 
 
 /** -----------------------------Drow functions here----------------------*/
-var iconSize = 64; // Width and height of tower icon
+var iconSize = 40; //Width and height of tower icon
 
-function clear() { // clear canvas 
+function clear() { //clear canvas 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawBg() { // render background of canvas
+function drawBg() { //render background of canvas
     ctx.drawImage(images.bg, 0, 0);
 }
 
-function drawTowersMenu() { // render towers icons in towers menu
+function drawTowersMenu() { //render towers icons in towers menu
 	ctx.drawImage(images.flameTowerIco, canvas.width-(iconSize+5), canvas.height-(iconSize+5));
 	ctx.drawImage(images.frostTowerIco, canvas.width-(iconSize+5)*2, canvas.height-(iconSize+5));
 }
@@ -66,11 +64,11 @@ function drawTowersMenu() { // render towers icons in towers menu
 function drawScene() {
 	clear();
 	drawBg();
-	if (drp == true) { // transparent bg, when new tower is building
+	if (build == true) { //transparent bg, when new tower is building
 		ctx.fillStyle = 'rgba(100, 190, 150, 0.3)';
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 	} 
-	if (typeof(newTower) != "undefined") { // draw tower that is building now
+	if (typeof(newTower) != "undefined") { //draw tower that is building now
 		ctx.drawImage(newTower.image, newTower.x, newTower.y);
 	}
 	for (var i = 0; i < towersArray.length; i++) {
@@ -91,6 +89,7 @@ function Tower(image, x, y, type, damage,  width, height)
 	this.type = type;
 	this.damage = damage;
 	this.level = 1;
+	this.radius = 50;
 	this.image = image;
 	this.x = x;
 	this.y = y;
@@ -104,30 +103,39 @@ initialization();
 setInterval(drawScene, 100);
 
 /** -----------------------------Events here--------------------------------*/
-canvas.onclick = function(e) { // mouseclick handle
-	var x, y;
-    x = e.clientX;
-    y = e.clientY;
-	if (x > 0 && x < canvas.width && y > 0 && y < canvas.height-(iconSize+5)) {
-		if (drp == true) {
-			drp = false;
-			towersArray.push(newTower);
+canvas.onclick = function(e) { //mouseclick handle
+	var mouseX, mouseY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+	if (mouseX > 0 && mouseX < canvas.width && mouseY > 0 && mouseY < canvas.height-(iconSize+5)) { 
+		if (build == true) { //If tower is built
+			build = false;
+			towersArray.push(newTower); //Add tower in array
+			delete newTower;
 		}
 	}
-	if (x > canvas.width-(iconSize+5) && x < canvas.width-5 && y > canvas.height-(iconSize+5) && y < canvas.height-5) {
-		if (drp == false) {
-			newTower = new Tower(images.flameTower, x, y, 'damage', 100);
-			drp = true;
+	if (mouseX > canvas.width-(iconSize+5) && mouseX < canvas.width-5 && mouseY > canvas.height-(iconSize+5) && mouseY < canvas.height-5)	towerType = 'flame'; //If tower icon clicked
+	if (mouseX > canvas.width-(iconSize+5)*2 && mouseX < canvas.width-(iconSize+5) && mouseY > canvas.height-(iconSize+5) && mouseY < canvas.height-5)	towerType = 'frost'; //If tower icon clicked
+	if (build == false && towerType != null) {
+		switch (towerType) {
+			case 'frost':
+				newTower = new Tower(images.frostTower, mouseX, mouseY, towerType, 100);
+				break;
+			case 'flame':
+				newTower = new Tower(images.flameTower, mouseX, mouseY, towerType, 150);
+				break;
 		}
+		build = true;
+		towerType = null;
 	}
 };
-canvas.onmousemove = function(e) { // mousemove handle
-	var x, y;
-	x = e.clientX;
-	y = e.clientY;
-	if (drp == true) {
-		newTower.x = x;
-		newTower.y = y;
+canvas.onmousemove = function(e) { //mousemove handle
+	var mouseX, mouseY;
+	mouseX = e.clientX;
+	mouseY = e.clientY;
+	if (build == true) {
+		newTower.x = mouseX;
+		newTower.y = mouseY;
 	}
 }
 
